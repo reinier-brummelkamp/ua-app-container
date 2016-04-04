e2c2f5b3750517078c16437b370910c4537cace7
Reinier Brummelkamp <reinier@urbanautomation.co.za>
2016-04-04 17:43:01 +0200

    Updated the README.md file and some sample code for a quickstart

---
+++ ElementsController.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Solitude.Controllers
+{
+    using Microsoft.AspNet.Mvc;
+
+    public class ElementsController : Controller
+    {
+        public IActionResult editUserDialog() { return View(); }
+
+
+
+        public IActionResult captainAmerica() { return View(); }
+        public IActionResult ironMan() { return View(); }
+        public IActionResult spiderMan() { return View(); }
+    }
+}
